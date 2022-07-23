6347786b8706a1fd1779cb993963bdd449096282
Caroline Wang <caroline.wang001@gmail.com>
2022-07-22 21:44:57 -0300

    final commit

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,33 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso extends Conteudo{
+
+    private int cargahoraria;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargahoraria;
+    }
+
+    public Curso() {
+    }
+
+    public int getCargahoraria() {
+        return cargahoraria;
+    }
+
+    public void setCargahoraria(int cargahoraria) {
+        this.cargahoraria = cargahoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", cargahoraria=" + cargahoraria +
+                '}';
+    }
+
+
+}
